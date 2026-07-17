c36c48c4049934804c44ca08078fc2fd2580596a
Khushi Tiwari <tiwari86424@gmail.com>
2026-07-17 17:07:20 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (39.36%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int BinarySub(const vector<int>& nums,int goal){
+        if(goal<0) return 0;
+        int n=nums.size();
+        int l=0;
+        int r=0;
+        int sum=0;
+        int count=0;
+        while(r<n){
+            sum+=nums[r];
+            while(sum>goal){
+              sum=sum-nums[l];
+              l++;
+            }
+            count+=(r-l+1);
+            r++;
+        }
+        return count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+
+        return BinarySub(nums,goal)-BinarySub(nums,goal-1);
+        
+    }
+};
