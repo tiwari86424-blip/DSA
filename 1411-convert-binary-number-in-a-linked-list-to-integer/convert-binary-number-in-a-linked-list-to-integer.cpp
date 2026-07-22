8509010844228fd5292fd4cb6d2e4ae3cca173a4
Khushi Tiwari <tiwari86424@gmail.com>
2026-07-22 19:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (94.60%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -10,39 +10,14 @@
  */
 class Solution {
 public:
-    int powerfun(int count){
-        int ans=1;
-        for(int i=0;i<count;i++){
-            ans*=2;
-        }
-        return ans;
-    }
-    // ListNode* reverse(ListNode* head){
-    //     if(head==nullptr || head->next==nullptr) return head;
-    //     ListNode* current=head;
-    //     ListNode* prev=nullptr;
-    //     while(current){
-    //         ListNode* front=current->next;
-    //         current->next=prev;
-    //         prev=current;
-    //         current=front;
-    //     }
-    //     return prev;
-    // }
+
     int getDecimalValue(ListNode* head) {
-        int count =0;
         ListNode* temp=head;
+        int ans=0;
         while(temp){
-            temp=temp->next;
-            count++;
-        }
-        int sum=0;
-        temp=head;
-        while(temp){
-          sum+=(temp->val*powerfun(count-1));
-          count--;
+          ans=ans*2 +temp->val;
           temp=temp->next;
         }
-        return sum;
+        return ans;
     }
 };
