d0de200828f20b3a7b3669aa05519ebcf173af37
Khushi Tiwari <tiwari86424@gmail.com>
2026-08-07 14:58:41 +0530

    Time: 43 ms (12.90%) | Memory: 42.7 MB (11.79%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     void Helper(int index,vector<int>& nums,vector<vector<int>>& ans,vector<int>ds,int target){
-        if(index==nums.size()){
+
         if(target==0){
             ans.push_back(ds);
+            return;
         }
-        return;
+        if(target<0 || index==nums.size()){
+            return;
         }
+        
         if(nums[index]<=target){
             ds.push_back(nums[index]);
             Helper(index,nums,ans,ds,target-nums[index]);
