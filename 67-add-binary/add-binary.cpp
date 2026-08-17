597fa4e51d96b2abfe66fff4ea39ce7b7a0664f1
Khushi Tiwari <tiwari86424@gmail.com>
2026-08-17 15:44:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (57.82%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -8,63 +8,18 @@ public:
         int c=0;
         string ans;
 
-        while(i>=0 && j>=0){
-            int sum=((a[i]-'0')+(b[j]-'0'))+c;
-            if(sum==0){
-                ans+='0';
-                c=0;
+        while(i>=0 || j>=0 || c){
+            int sum=c;
+            if(i>=0){
+                sum+=a[i--]-'0';
             }
-            else if(sum==1){
-                ans+='1';
-                c=0;
+            if(j>=0){
+                sum+=b[j--]-'0';
             }
-            else if(sum==2){
-                ans+='0';
-                c=1;
-            }
-            else {
-                ans+='1';
-                c=1;
-            }
-            i--;
-            j--;
+            ans+=(sum%2)+'0';
+            c=sum/2;
         }
-        while(i>=0){
-            int sum=(a[i]-'0')+c;
-            if(sum==0){
-               ans+='0';
-               c=0;
-            }
-            else if(sum==1){
-                ans+='1';
-                c=0;
-            }
-            else{
-                ans+='0';
-                c=1;
-            }
-            i--;
-        }
-
-        while(j>=0){
-            int sum=(b[j]-'0')+c;
-            if(sum==0){
-               ans+='0';
-               c=0;
-            }
-            else if(sum==1){
-                ans+='1';
-                c=0;
-            }
-            else{
-                ans+='0';
-                c=1;
-            }
-            j--;
-        }
-        if(c==1){
-            ans+='1';
-        }
+            
         reverse(ans.begin(),ans.end());
         return ans;
         
