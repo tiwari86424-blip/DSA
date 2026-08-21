4d5b74fdbf30912ab3ecd2c4043b0f1a3db15415
Khushi Tiwari <tiwari86424@gmail.com>
2026-08-21 15:08:19 +0530

    Time: 2 ms (80.64%) | Memory: 13.1 MB (83.15%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void CreateParenthesis(int n,int ob,int cb,vector<string>& ans,string&ds){
+        if(ob==n && cb==n){
+            ans.push_back(ds);
+        }
+        if(ob<n){
+            ds +='(';
+            CreateParenthesis(n,ob+1,cb,ans,ds);
+            ds.pop_back();
+        }
+        if(ob>cb){
+            ds +=')';
+            CreateParenthesis(n,ob,cb+1,ans,ds);
+            ds.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        string ds;
+        vector<string>ans;
+        CreateParenthesis(n,0,0,ans,ds);
+        return ans;
+    }
+};
